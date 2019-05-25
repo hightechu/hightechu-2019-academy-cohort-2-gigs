a8b1738e5260048959ad6e7972a64018e315c7a8
lucaswil <lucaswilsoncontact@gmail.com>
2019-05-25 01:17:13 -0700

    Hurrah! Pretty well entirely functional now! (With relatively upgraded readability)

--- js/javascript.js
+++ js/javascript.js
@@ -4,35 +4,34 @@ var txt2 = 'One-off jobs made easy';
 var speed = 90;
 
 function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("demo").innerHTML += txt.charAt(i);
-	if (i == txt.length - 1 ) {
-	  speed = 2000;
-	}
-  }
-  else if(i<txt.length*2){
-      document.getElementById("demo").innerHTML = txt.substring(0,txt.length*2-i-1);
-	  speed = 90;
-  }
-   else if(i<txt.length*2+txt2.length){
-       speed = 90;
-       document.getElementById("demo").innerHTML += txt2.charAt(i-txt.length*2);
-		  if (i == txt.length*2+txt2.length - 1 ) {
-        speed = 2000;
-      }
-   }
-   else if(i<txt.length*2+txt2.length*2){
-      speed = 90;
-      document.getElementById("demo").innerHTML = txt2.substring(0,txt2.length*2-i-1+txt.length*2);
-	  	if (i == txt.length - 1 ) {
-	  speed = 2000;
-	}
-   }
-   else{i=-1, speed = 90;}
-      i++;
+    if (i < txt.length) {
+        document.getElementById("demo").innerHTML += txt.charAt(i);
+        if (i == txt.length - 1) {
+            speed = 2000;
+        }
+    } else if (i < txt.length * 2) {
+        document.getElementById("demo").innerHTML = txt.substring(0, txt.length * 2 - i - 1);
+        speed = 90;
+    } else if (i < txt.length * 2 + txt2.length) {
+        speed = 90;
+        document.getElementById("demo").innerHTML += txt2.charAt(i - txt.length * 2);
+        if (i == txt.length * 2 + txt2.length - 1) {
+            speed = 2000;
+        }
+    } else if (i < txt.length * 2 + txt2.length * 2) {
+        speed = 90;
+        document.getElementById("demo").innerHTML = txt2.substring(0, txt2.length * 2 - i - 1 + txt.length * 2);
+        if (i == txt.length - 1) {
+            speed = 2000;
+        }
+    } else {
+        i = -1, speed = 90;
+    }
+    i++;
     setTimeout(typeWriter, speed);
-	
+
 }
+
 function myFunction(x) {
-  x.classList.toggle("change");
+    x.classList.toggle("change");
 }
