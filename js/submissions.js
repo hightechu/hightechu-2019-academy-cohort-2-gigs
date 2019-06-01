a11f022a527df7a088ca49ab2e08dc47bcbd9dca
hightechu2 <hightechu2@uvic.ca>
2019-06-01 14:13:46 -0700

    did the back end! it works yay :P

--- js/submissions.js
+++ js/submissions.js
@@ -11,9 +11,18 @@ function loadSubmissionsAJAX() {
             if('error' in response) {
                 alert(response.error.message);
             }
-            else {
-                alert("Got profile" + response);
-            }
+                text="";
+                for (userprofile of response){
+                    console.log(userprofile);
+                    text += '<div class="rectangle-left">';
+                    text += '<h2 class="profilename-left">'+userprofile.FirstName+'</h2>';
+                    text += '<p class="profileinfo-left">'+userprofile.AboutMe+'</p>';
+                    text += '<p class ="profilecontact-left">'+userprofile.PhoneNumber+'</p>';  
+                    text += '<p class ="profilecontact-left">'+userprofile.Email+'</p>';
+                    text += '</div>';
+                }
+                    var profilediv = document.getElementById("profiles")
+                    profilediv.innerHTML = text;
         }
     }
 
